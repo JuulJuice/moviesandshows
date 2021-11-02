f8e8137a4c074864fbd2bc21da66c86b7da728df
Julian Jackson <taz38877@msn.com>
2021-11-02 09:45:23 -0400

    yeaaa

--- moviesandshows/ViewController.swift
+++ moviesandshows/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    var images = [#imageLiteral(resourceName: "regular_show"), #imageLiteral(resourceName: "regular_show_movie"), #imageLiteral(resourceName: "scott_pilgrim"), #imageLiteral(resourceName: "beastars"), #imageLiteral(resourceName: "demon_slayer") ]
+    var titles = ["Regular Show", "Regular Show: The Movie", "Scott Pilgrim vs The World", "Beastars", "Demon Slayer"]
+    @IBOutlet weak var heading: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+var position = 0
+    @IBAction func next(_ sender: Any) {
+        if(position < images.count){
+                    heading.text = titles [position]
+                    imageView.image = images[position]
+                    position += 1
+             }
+         else{
+                    position = 0
+                                       
+                }
 
-
+        
+    }
+    
 }
 
